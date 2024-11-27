6574804969e8c974b56570bc13fe88f7aed3f991
Jhon Inofuente <jhonjb90@gmail.com>
2024-11-26 22:51:25 -0500

    se implenta con dos funciones

--- src/main/java/pe/dhexsoft/api_rest/controller/GreetingController.java
+++ src/main/java/pe/dhexsoft/api_rest/controller/GreetingController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pe.dhexsoft.api_rest.service.GreetingService;
+import pe.dhexsoft.api_rest.service.impl.GreetingServiceImpl2;
 
 @RestController
 @RequestMapping("/greet")
@@ -13,13 +14,24 @@ public class GreetingController {
 
     //ahora si lo quieres hacer con el autowired. seria de esta forma, agregando tambien tu "@qualifier"
     @Autowired
-    @Qualifier("greetingServiceImpl2")
+    @Qualifier("greetingServiceImpl")
     private GreetingService greetingService;
 
 
-    @GetMapping()
-    public String greet(){
+    private final GreetingService greetingService2;
+    public GreetingController(@Qualifier("greetingServiceImpl2") GreetingService greetingService2) {
+        this.greetingService2 = greetingService2;
+    }
+
+
+    @GetMapping("/gt1")
+    public String greet1(){
         return greetingService.greet();
+    }
+
+    @GetMapping("/gt2")
+    public String greet2(){
+        return greetingService2.greet();
     }
 
 
